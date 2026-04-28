445a45180c346ce040ae1963d13755c5c5a38cbd
Preston <prestonrbattle@gmail.com>
2026-04-28 09:48:03 -0400

    Comment out unused background shape elements for cleaner code

--- src/global/components/background.tsx
+++ src/global/components/background.tsx
@@ -8,8 +8,8 @@ export function Background() {
         animation: "rotate 20s alternate infinite",
       }}
     >
-      <div className="[clip-path:polygon(0_0,100%_0,100%_80%,20%_40%,0_80%)] bg-(--mantine-color-purple-7) h-96"></div>
-      <div id="background-shape-1" className="w-9/12 h-10/12 bottom-6/12 right-6/12 bg-(--mantine-color-purple-7) rounded-full absolute opacity-30"></div>
+      {/* <div className="[clip-path:polygon(0_0,100%_0,100%_80%,20%_40%,0_80%)] bg-(--mantine-color-purple-7) h-96"></div>
+      <div id="background-shape-1" className="w-9/12 h-10/12 bottom-6/12 right-6/12 bg-(--mantine-color-purple-7) rounded-full absolute opacity-30"></div> */}
       <div className="absolute inset-0 backdrop-blur-xl"></div>
     </div>
   )
